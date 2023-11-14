a0954946eb69c583e00dd1ecb71d02e1d5895105
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-11-13 19:49:53 -0500

    Ejercicio terminado, queda pendiente modificar la imagen en GitHub mismo.

---
+++ JavaScript/Reto-VariablesTiposDatos/README.md
@@ -0,0 +1,3 @@
+# Reto: Variables y tipos de datos
+1. Intenta escribir el código de JavaScript que ves en la consola. No tiene que ser perfecto! 🧑‍💻
+2. Compara tu solución con la solución incluida en los archivos y descubre que partes pudiste hacer de mejor manera. No te preocupes si encuentras métodos que no conocías, ya las conocerás! 🙌 Buenas habilidades! 🔥
---
+++ JavaScript/Reto-VariablesTiposDatos/index.js
@@ -0,0 +1,49 @@
+let welcome = "¡Hola Mundo!";
+let nombre = "Fernando";
+
+// 1° linea de saludo y nombre:
+console.log(welcome + " Soy " + nombre);
+
+// 2° linea nombre en mayusculas:
+let nombreMayus = nombre.toUpperCase();
+console.log("Mi nombre en mayúsculas es: " + nombreMayus);
+
+// 3° linea nombre en minusculas:
+let nombreMin = nombre.toLowerCase();
+console.log("Mi nombre en minúsculas es: " + nombreMin);
+
+// 4° linea conteo de caracteres del mensaje:
+console.log(
+  "El mensaje de '" + welcome + "' tiene " + welcome.length + " caracteres."
+);
+
+// 5° linea número del array:
+let array = [1, 2, 3, 4, 5];
+let thirdNum = 0;
+for (let index = 0; index < array.length; index++) {
+    if (index = 2) {
+        thirdNum = array[index];
+        break;
+    }
+}
+console.log(
+  "El tercer número del array con los valores '" + array + "' es: " + thirdNum
+);
+
+// 6° linea añadir número al final del array:
+array = array.concat(6);
+let lastNum = 0;
+for (let index = 0; index < array.length; index++) {
+  if ((index = array.length - 1)) {
+    lastNum = array[index];
+  }
+}
+console.log(
+  "Se añade el número que sigue al final del arreglo y es: " + lastNum
+);
+
+// 7° linea encontrar la palabra en una cadena de texto.
+let cadena = "'La lluvia en Sevilla es una maravilla.'";
+let plabra = "lluvia";
+let busqueda = cadena.match(/\blluvia\b/);
+console.log("Se encontró el patrón '" + plabra + "' en el texto " + cadena + ", por lo que el resultado es: " + busqueda);
