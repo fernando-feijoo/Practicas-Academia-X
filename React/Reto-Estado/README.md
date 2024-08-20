ddbaec0af25574b81717c7b5cf8230c526bc7ffa
Fernando PC-Escritorio <fernando95f4@gmail.com>
2024-08-20 17:56:01 -0500

    Se mejoro el README.md y coloco link para ver despliegue de la app o reto. Ademas de haber cambiado el page en vite, para hacer build con rutas relativas y no absolutas.

--- React/Reto-Estado/README.md
+++ React/Reto-Estado/README.md
@@ -1,6 +1,9 @@
 # Reto: Estado
 
+`<link>` [Ver despliegue de reto](https://react.tfeijoo.com/reto-estado/) 👈🏻
+
 1. Crea un componente de tarjetas de productos el cual incluya una imagen, un titulo, una descripción y contador con la cantidad de artículos de ese producto. 🧠
 2. Intenta escribir el código de JSX y CSS que ves en esta página renderizada. No tiene que ser perfecto! 🧑‍💻
 3. Compara tu solución con la solución incluida en los archivos descargables al inicio del curso y descubre que partes pudiste hacer de mejor manera. No te preocupes si encuentras métodos que no conocías, ya las conocerás! 🙌 Buenas habilidades! 🔥
 
+![Imagen Reto](https://raw.githubusercontent.com/fernando-feijoo/Practicas-Academia-X/master/React/Reto-Estado/src/assets/ilustracion.png)
--- React/Reto-Estado/src/main.jsx
+++ React/Reto-Estado/src/main.jsx
@@ -2,24 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Tarjeta from "./components/Tarjeta.jsx";
 import "./css/global.css";
+import myImage1 from "./assets/mando.webp";
+import myImage2 from "./assets/mando-scuff.webp";
+import myImage3 from "./assets/xbox-scuff.jpg";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="contenedor-tarjetas">
       <Tarjeta
-        imgSrc="./src/assets/mando.webp"
+        imgSrc={myImage1}
         imgAlt="Imagen de producto."
         titulo="Mando DualSense - PS5"
         descripcion="El mando Dualesense está optimizado para el gaming colaborativo o competitivo online, gracias a su micrófono integrado y su capacidad para utilizar auriculares por cable o bluetooht, incluyendo un botón específico de silencio para desactivar el micro fácilmente en cualquier momento."
       />
       <Tarjeta
-        imgSrc="./src/assets/mando-scuff.webp"
+        imgSrc={myImage2}
         imgAlt="Imagen de producto."
         titulo="Scuf Reflex Pro - PS5"
         descripcion="Probamos a fondo el mando Scuf Reflex compatible con PS5 y PC, un modelo que ahora admite unas elevadas opciones de personalización en su diseño, aunque la característica estrella son las palancas traseras que añaden cuatro botones extra, siendo una de las mejores alternativas para quienes buscan un mando Pro de PS5."
       />
       <Tarjeta
-        imgSrc="./src/assets/xbox-scuff.jpg"
+        imgSrc={myImage3}
         imgAlt="Imagen de producto."
         titulo="Scuf Prestige - Xbox One"
         descripcion="El mundo de los mandos competitivos no se detiene. La marca Scuf, conocida por haber modificado mandos como el DualShock para poder ofrecer nuevas experiencias en consolas como Playstation 4, cuenta con una línea premium basada en Prestige, Vantage y e Infinity. En este artículo nos vamos a centrar en el primero, diseñado para Xbox One y que se convierte en una propuesta interesante para los que quieran un mando centrado en experiencias competitivas como puede ser el Elite."
