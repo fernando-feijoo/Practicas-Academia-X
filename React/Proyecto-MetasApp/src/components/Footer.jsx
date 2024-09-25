5f66c8a75bf1c446dc09698be86055dff84f5238
Fernando PC-Portatil <fernando95f4@gmail.com>
2024-09-25 15:36:53 -0500

    Primer commit, con librería de taildwindcss.

---
+++ React/Proyecto-MetasApp/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="text-red-500">
+      <small>Desarrollado y diseñado por Fernando Feijoo © 2024. Todos los derechos reservados.</small>
+    </footer>
+  );
+}
+
+export default Footer;
