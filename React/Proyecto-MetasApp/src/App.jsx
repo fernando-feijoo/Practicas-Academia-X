6c7bc4e16ab3d6cef3cb3bb13d4f789bcc3f3b54
Fernando PC-Portatil <fernando95f4@gmail.com>
2024-11-01 18:19:14 -0500

    Estructura base de Metas

--- React/Proyecto-MetasApp/src/App.jsx
+++ React/Proyecto-MetasApp/src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './components/shared/Header.jsx';
 import Main from './components/shared/Main.jsx';
 import Footer from './components/shared/Footer.jsx';
+import Meta from './components/list/Meta.jsx';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Main></Main>
+      <Main>
+        <Meta></Meta>
+      </Main>
       <Footer></Footer>
     </>
   );
